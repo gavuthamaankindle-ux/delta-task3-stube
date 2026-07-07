45552682bbb941b40810bad47021deb59a7804cf
gavuthamaankindle-ux <gavuthamaankindle@gmail.com>
2026-07-07 22:31:46 +0530

    Refactor protected routes and add new endpoints

--- server/routes/protectedroutes.go
+++ server/routes/protectedroutes.go
@@ -10,9 +10,16 @@ import (
 func ProtectedRoutes(router *gin.Engine, client *mongo.Client) {
 	router.Use(middleware.AuthMiddleWare())
 
-	 router.GET("/video/:id", controllers.Getvideo(client))
-	 router.PATCH("/video/:id/like", controllers.Addlikes(client))
-	 router.PATCH("/video/:id/removelike", controllers.Removelikes(client))
-     router.PATCH("/video/:id/addcomment", controllers.Addcomment(client))
-	 router.GET("/channel/:channelid", controllers.GetVideosByChannel(client))
+	 router.Use(middleware.AuthMiddleWare())
+	router.PATCH("/video/:id/like", controllers.Addlikes(client))
+	router.PATCH("/video/:id/removelike", controllers.Removelikes(client))
+	router.POST("/video/request-upload", controllers.RequestVideoUpload(client))
+	router.GET("/video/:id", controllers.Getvideo(client))
+	router.PATCH("/channel/:id/subscribe", controllers.Subscribe(client))
+	router.GET("/ban-user/:id", controllers.BanUser(client))
+	router.GET("/users", controllers.Getusers(client))
+	router.POST("/unban-user/:id", controllers.UnBanUser(client))
+	router.GET("/video/:id", controllers.Getvideo(client))
+	router.GET("/channel/:channelid", controllers.GetVideosByChannel(client))
+	router.PATCH("/video/:id/addcomment", controllers.Addcomment(client))
 }
