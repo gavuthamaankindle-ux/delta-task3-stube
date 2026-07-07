372011dcf1422ed4115a002b6690226407893874
gavuthamaankindle-ux <gavuthamaankindle@gmail.com>
2026-07-07 22:22:20 +0530

    Uncomment protected route handlers

--- server/routes/protectedroutes.go
+++ server/routes/protectedroutes.go
@@ -10,9 +10,9 @@ import (
 func ProtectedRoutes(router *gin.Engine, client *mongo.Client) {
 	router.Use(middleware.AuthMiddleWare())
 
-	// router.GET("/video/:id", controllers.Getvideo(client))
-	// router.PATCH("/video/:id/like", controllers.Addlikes(client))
-	// router.PATCH("/video/:id/removelike", controllers.Removelikes(client))
-	// router.PATCH("/video/:id/addcomment", controllers.Addcomment(client))
-	//router.GET("/channel/:channelid", controllers.GetVideosByChannel(client))
+	 router.GET("/video/:id", controllers.Getvideo(client))
+	 router.PATCH("/video/:id/like", controllers.Addlikes(client))
+	 router.PATCH("/video/:id/removelike", controllers.Removelikes(client))
+     router.PATCH("/video/:id/addcomment", controllers.Addcomment(client))
+	 router.GET("/channel/:channelid", controllers.GetVideosByChannel(client))
 }
